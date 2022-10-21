eea0ad6225c8a7d276019b14765bb4a49c1bb74a
Anabel <akinsey808@users.noreply.github.com>
2022-10-21 02:57:53 +0000

    Name of 2nd box

--- next-site/pages/index.js
+++ next-site/pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2>How the Internet Reaches Hawai'i &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
