e49f366e9cd547f819cfb0b5087758adbbca4eb9
Anabel <akinsey808@users.noreply.github.com>
2022-10-21 23:41:06 +0000

    arrow

--- next-site/pages/access.js
+++ next-site/pages/access.js
@@ -1,16 +1,13 @@
 import styles from "./Access.module.css";
 import Image from 'next/image';
-import arrow from './Arrow.png';
-
-<Image 
-  src={arrow}
-  width="50px"
-  height="50px"
-/>
+import BackArrow from './arrow.svg';
 
 export default function Access() {
   return (
+    <>
+    <BackArrow className={styles.backArrow}/>
     <div className={styles.access}> <strong>How to Access the Internet</strong>
     </div>
+    </>
   )
 }
