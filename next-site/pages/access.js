d821a57588903eafc2f5ed423ccc529d3df2ebec
Anabel <akinsey808@users.noreply.github.com>
2022-10-22 00:10:38 +0000

    access images

--- next-site/pages/access.js
+++ next-site/pages/access.js
@@ -1,13 +1,19 @@
 import styles from "./Access.module.css";
 import Image from 'next/image';
-import BackArrow from './arrow.svg';
+import BackArrow from './images/arrow.svg';
+import Web from './images/web.svg';
+import Search from './images/search.svg';
 
 export default function Access() {
   return (
     <>
-    <BackArrow className={styles.backArrow}/>
-    <div className={styles.access}> <strong>How to Access the Internet</strong>
-    </div>
+      <a href="..">
+        <BackArrow className={styles.backArrow}/>
+      </a>
+      <div className={styles.access}> <strong>How to Access the Internet</strong>
+      </div>
+      <Search className={styles.search}/>
+      <Web className={styles.web}/>
     </>
   )
 }
--- next-site/pages/index.js
+++ next-site/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Sun from './Sun.svg';
+import Sun from './images/Sun.svg';
 
 export default function Home() {
   return (
