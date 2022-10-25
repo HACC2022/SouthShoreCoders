005250b3a5b69b0c37a7aaf619dfd172bbbe1416
Ben Kinsey <bkinsey@gmail.com>
2022-10-24 20:43:44 -1000

    image example

--- next-site/pages/access.js
+++ next-site/pages/access.js
@@ -1,36 +1,67 @@
 import styles from "./Access.module.css";
-import Image from 'next/image';
-import BackArrow from './images/arrow.svg';
-import Web from './images/web.svg';
-import Search from './images/search.svg';
-import InternetSymbol from './images/internet.svg';
+import BackArrow from "./images/arrow.svg";
+import Web from "./images/web.svg";
+import Search from "./images/search.svg";
+import InternetSymbol from "./images/internet.svg";
 
-console.log("The Secret Code is BANANA")
+// import Image from "next/image";
+// import diagram from "./images/diagram.jpg";
+
+console.log("The Secret Code is BANANA");
 
 export default function Access() {
   return (
     <div className={styles.all}>
       <a href="..">
-        <BackArrow className="backArrow"/>
+        <BackArrow className="backArrow" />
       </a>
-      <div className={styles.access}> <strong>How to Access the Internet</strong>
+      <div className={styles.access}>
+        {" "}
+        <strong>How to Access the Internet</strong>
       </div>
-      <p className={styles.opener}>To access the Internet, open any web browser, such as Google  Chrome or Internet Explorer.  Make sure you are connected to a  Wifi network by pressing this button: <InternetSymbol className={styles.internet}/>, usually in the top left or bottom right of the screen. </p>
-      <Search className={styles.search}/>
+      <p className={styles.opener}>
+        To access the Internet, open any web browser, such as Google Chrome or
+        Internet Explorer. Make sure you are connected to a Wifi network by
+        pressing this button: <InternetSymbol className={styles.internet} />,
+        usually in the top left or bottom right of the screen.{" "}
+      </p>
+      <Search className={styles.search} />
       <p>When you search for "cute cat pics"...</p>
-      <Web className={styles.web}/>
-      <p>A complex network of servers and computers finds the best mathces. You can learn more about this in the How the Internet Works section of this webpage.</p>
+      <Web className={styles.web} />
+      <p>
+        A complex network of servers and computers finds the best mathces. You
+        can learn more about this in the How the Internet Works section of this
+        webpage.
+      </p>
       <p>And ta-da! Cute cat pics!</p>
-      <p>You can look up anything that is important or interesting to you, as long as it is appropriate fo rthe time and place.</p>
-      <p>For example:<br></br> Free Tutoring near me<br></br>Dog-friendly parks<br></br>Scholarships due in January<br></br>Math practice online</p>
-    <p>The Internet can be an amazing resource for education and enrichment if used properly.</p>
-      <p>Do you  want to see what makes a webpage run?</p>
+      <p>
+        You can look up anything that is important or interesting to you, as
+        long as it is appropriate fo rthe time and place.
+      </p>
+      <p>
+        For example:<br></br> Free Tutoring near me<br></br>Dog-friendly parks
+        <br></br>Scholarships due in January<br></br>Math practice online
+      </p>
+      <p>
+        The Internet can be an amazing resource for education and enrichment if
+        used properly.
+      </p>
+      <p>Do you want to see what makes a webpage run?</p>
       <p>You can right click on your keyboard or mouse and press "Inspect." </p>
-      <p>Wow! You can see all of the source code. Pretty  cool. There will be a secret message hidden in the code of this webpage! Did you find it?</p>
-      <p>If you are interested in learning more about how this works, a great resorce  to learn coding is <a href="https://www.codecademy.com/">Codecademy.</a>
-</p>
-      <p>You've reached the end! Have fun using the Internet in useful ways and taking it to the next level.</p>
-      <Image src="./images/diagram" width="2rem" height="2rem" />
+      <p>
+        Wow! You can see all of the source code. Pretty cool. There will be a
+        secret message hidden in the code of this webpage! Did you find it?
+      </p>
+      <p>
+        If you are interested in learning more about how this works, a great
+        resorce to learn coding is{" "}
+        <a href="https://www.codecademy.com/">Codecademy.</a>
+      </p>
+      <p>
+        You've reached the end! Have fun using the Internet in useful ways and
+        taking it to the next level.
+      </p>
+      {/* <Image src={diagram} width="480px" height="480px" /> */}
     </div>
-  )
+  );
 }
