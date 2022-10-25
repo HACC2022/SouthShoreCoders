da52a841a7c86246fe5bc702ee6e3943ba741976
Hoku <116043474+hokud@users.noreply.github.com>
2022-10-25 03:58:57 +0000

    internet page

--- next-site/pages/internet.js
+++ next-site/pages/internet.js
@@ -1,5 +1,6 @@
 import styles from "./internet.module.css";
 import BackArrow from "./images/arrow.svg";
+import Diagram from './images/diagram.jpg';
 
 export default function Internet() {
   return (
@@ -7,7 +8,9 @@ export default function Internet() {
       <a href="..">
         <BackArrow className="backArrow" />
       </a>
-      <h1 className={pages.title}>How the Internet Works</h1>
+      <h1 className={styles.title}>How the Internet Works</h1>
+      <p className={styles.intro}>Let’s say you’re trying to send an email from your computer to your friend’s computer. Click on each number to see how the information travels from your computer to reach your friends computer through the internet.</p>
+
     </>
-  );
+  )
 }
